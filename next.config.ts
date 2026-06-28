36f4578c8210d936ffb4ee7fb16a99a932e55748
mariaeduardafae-beep <mariaeduardafae@xtyl.digital>
2026-06-28 17:18:15 -0300

    Fix Supabase image hostname

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "pvozitbyskyttbwurdm.supabase.co"
+        hostname: "pvozlrbypkvytfbwurdm.supabase.co"
       }
     ]
   }
